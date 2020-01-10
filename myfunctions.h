80c906b46c7f5c892e69164ef18620d05c3630f5
nirmian <mirondorin99@gmail.com>
2020-01-10 23:16:04 +0200

    added for now

---
+++ myfunctions.h
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sqlite3.h>
+
+
+/*
+int showAll(int rc, sqlite3 *db) {
+    
+    char *sql = "SELECT * FROM Songs";
+    char *err_msg = 0;
+        
+    rc = sqlite3_exec(db, sql, callback, 0, &err_msg);
+    
+    if (rc != SQLITE_OK ) {
+        
+        fprintf(stderr, "Failed to select data\n");
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+        
+        return 1;
+    }
+}
+
+int callback(void *NotUsed, int argc, char **argv, 
+                    char **azColName) {
+    
+    NotUsed = 0;
+    
+    for (int i = 0; i < argc; i++) {
+
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    
+    printf("\n");
+    
+    return 0;
+} */
